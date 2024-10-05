803fc83ed98043bb1ca25732341a65b865ffdc7b
Aayushshah <shahayush688@gmail.com>
2024-10-05 16:08:31 +0530

    util file has been updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,13 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const registerUser=asyncHandler(async(req,res)=>{
-    res.status(200)
-    .json({
-        message:"i love radha "
-    })
+    // get user details from frontend 
+    //validation - not empty
+    //check if user all ready exit 
+    //check for images ,check for avatar 
+    //uploaded them to cloudinary 
+    //create user object - create entry in db 
+    //remove password and refresh token field from response 
+    //check for user creation 
+    //return res
 })
