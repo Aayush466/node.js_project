4d9d5aeab080f6242f8e4e3f1a87411c0f0c2a9e
Aayushshah <shahayush688@gmail.com>
2024-10-06 11:23:42 +0530

    register has been added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,7 +24,7 @@ export const registerUser=asyncHandler(async(req,res)=>{
         throw new ApiError(400,"All field are required ")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[
             {email},
             {userName}
@@ -34,8 +34,15 @@ export const registerUser=asyncHandler(async(req,res)=>{
         throw new ApiError(409,"User already exist")
     }
 
+    console.log(req.files);
+    
     const avatarLocalpath = req.files?.avatar[0]?.path
-    const coverImageLocalpath = req.files?.coverImage[0]?.path
+    // const coverImageLocalpath = req.files?.coverImage[0]?.path
+
+    let coverImageLocalpath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalpath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalpath){
         throw new ApiError(400,"Avatar is required")
@@ -68,3 +75,4 @@ export const registerUser=asyncHandler(async(req,res)=>{
         new ApiResponse(200,createdUser,"userCreated successfully ")
     )
 })
+
